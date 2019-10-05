04fe1654ac625150d891e9591b9883fecbd1c3b4
Sam Green <sam.bg@hotmail.com>
2019-10-06 10:01:53 +1300

    Added a background image with a transparent background colour overlay

--- vue-website-src/src/App.vue
+++ vue-website-src/src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <Nav></Nav>
   </div>
+  
 </template>
 
 <script>
@@ -16,12 +17,55 @@ export default {
 </script>
 
 <style>
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html, body {
+   height: 100%;
+   width: 100%;
+}
+
+#app::before {
+  background-image: url(./assets/background.jpg);
+  background-size: cover;
+  content: "";
+  display: block;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: -2;
+  opacity: 0.5;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  width: 100%;
+  height: 100%;
+  min-width: 100%;
+  min-height: 100%;
+  position: relative;
+}
+
+#app::after {
+  background-color: #DDA0DD;
+  content: "";
+  display: block;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+  z-index: -1;
+  opacity: 0.6;
 }
 </style>
